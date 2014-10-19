db2e9da27c12d53f902712a97889fabe27d7c2ea
typhaon <--global>
2014-10-19 19:37:00 -0400

    Initial Commit

---
+++ testserver.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+
+var server = http.createServer(function (request, response) {
+	response.writeHead(200, {"content-Type": "text/plain"});
+	response.end("Hello World\n");
+});
+
+server.listen(8000);
+
+console.log("Server running at http://127.0.0.1:8000/");
+
+
+
